d59338380e60019ff9a06b8010a22eafe313b6ff
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2026-01-11 21:05:17 +0530

    added url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://api-box-delta.vercel.app"
 })
 
 export const { signIn, useSession } = createAuthClient()
